b15e485d9bff0f59b5c8893c314d03737ec5a075
W3NDO <wendonyang@gmail.com>
2022-10-02 07:09:05 +0300

    feat(main): Can track starting time now.

--- app.rb
+++ app.rb
@@ -3,12 +3,35 @@
 #This is a work timer. It takes stock of how much time I spend at work. IDK how else to explain that
 
 require "sinatra/base"
+require 'json'
 
 
 class Application < Sinatra::Base
-    get '/' do
-        "hello world"
+    def set_start_time(id)
+        times = JSON.parse(File.read("./tmp/start_time.txt"))
+        if times[id]
+            return times[id]
+        else
+            times[id] = Time.now
+            File.write("./tmp/start_time.txt", JSON.dump(times) )
+            times[id]
+        end
+        File.write("./tmp/start_time.txt", "#{{id.to_sym => Time.now}.to_json}" )
     end
 
-    
+    get '/' do
+        params[:id] = rand(1000..9999)
+        p "params #{params}"
+        erb :index, :layout => :default_layout
+    end
+
+    post '/' do
+        set_start_time(params[:id])
+        erb :index, :layout => :default_layout
+    end
+
+    get '/history' do
+        "history"
+    end
+
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -14,9 +14,31 @@ RSpec.describe Application do
             expect(response.status).to eq 200
         end
 
-        it "gets the root path" do
-            expect(response.body).to include "hello world"
+        it "returns page content" do
+            expect(response.body).to include "Wendo's Basic Work Timer"
         end
+    end
 
+    context "POST to root path(/)" do
+        let(:response) {post "/"}
+        it "starts new timer and sends back the starting time" do
+            expect(response.body[start_time]).to be < Time.now
+        end
+    end
+
+    context "GET to /recap" do
+        it "navigates to work recap form page"
+    end
+
+    context "GET to /recap?id=[:id]" do
+        it "navigates to work recap for that specific work item"
+    end
+
+    context "GET to /history" do
+        it "navigates to history page"
+    end
+
+    context "POST to /history" do
+        it "returns a list of previous work time"
     end
 end
